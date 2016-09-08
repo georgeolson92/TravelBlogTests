ade7411c6f161e8ba5b6a4eae06561694185a96a
George Olson <georgeolson92@gmail.com>
2016-09-08 14:37:43 -0700

    add styling

---
+++ src/travelblog/Migrations/20160908201841_AddImagesAndDescriptions.Designer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TravelBlog.Models;
+
+namespace travelblog.Migrations
+{
+    [DbContext(typeof(TravelBlogDbContext))]
+    [Migration("20160908201841_AddImagesAndDescriptions")]
+    partial class AddImagesAndDescriptions
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.Property<int>("ExperienceId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("ImageURL");
+
+                    b.Property<int>("LocationId");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ExperienceId");
+
+                    b.HasIndex("LocationId");
+
+                    b.ToTable("Experiences");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Location", b =>
+                {
+                    b.Property<int>("LocationId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CityName");
+
+                    b.Property<string>("Country");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("ImageURL");
+
+                    b.HasKey("LocationId");
+
+                    b.ToTable("Locations");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.People", b =>
+                {
+                    b.Property<int>("PersonId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Bio");
+
+                    b.Property<string>("ImageURL");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("PersonId");
+
+                    b.ToTable("People");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.PeopleExperience", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ExperienceId");
+
+                    b.Property<int>("PersonId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ExperienceId");
+
+                    b.HasIndex("PersonId");
+
+                    b.ToTable("PeopleExperiences");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.PeopleLocation", b =>
+                {
+                    b.Property<int>("id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("LocationId");
+
+                    b.Property<int>("PersonId");
+
+                    b.HasKey("id");
+
+                    b.HasIndex("LocationId");
+
+                    b.HasIndex("PersonId");
+
+                    b.ToTable("PeopleLocations");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.HasOne("TravelBlog.Models.Location")
+                        .WithMany()
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.PeopleExperience", b =>
+                {
+                    b.HasOne("TravelBlog.Models.Experience")
+                        .WithMany()
+                        .HasForeignKey("ExperienceId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("TravelBlog.Models.People")
+                        .WithMany()
+                        .HasForeignKey("PersonId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.PeopleLocation", b =>
+                {
+                    b.HasOne("TravelBlog.Models.Location")
+                        .WithMany()
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("TravelBlog.Models.People")
+                        .WithMany()
+                        .HasForeignKey("PersonId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ src/travelblog/Migrations/20160908201841_AddImagesAndDescriptions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace travelblog.Migrations
+{
+    public partial class AddImagesAndDescriptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Experiences",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Experiences",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Experiences");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Experiences");
+        }
+    }
+}
